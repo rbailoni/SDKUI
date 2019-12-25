7db89fc4e20dbb4245da37cad3322e32c7c242d6
Ricardo Bailoni <rbailoni@rbailoni.com.br>
2019-12-25 01:43:22 -0300

    Initial project setup

---
+++ SDKUI/SDKUI.h
@@ -0,0 +1,19 @@
+//
+//  SDKUI.h
+//  SDKUI
+//
+//  Created by Ricardo Bailoni on 25/12/19.
+//  Copyright © 2019 Ricardo Bailoni. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SDKUI.
+FOUNDATION_EXPORT double SDKUIVersionNumber;
+
+//! Project version string for SDKUI.
+FOUNDATION_EXPORT const unsigned char SDKUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDKUI/PublicHeader.h>
+
+
---
+++ SDKUIExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SDKUIExamples
+//
+//  Created by Ricardo Bailoni on 25/12/19.
+//  Copyright © 2019 Ricardo Bailoni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
